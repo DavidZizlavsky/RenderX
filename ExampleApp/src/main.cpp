81dfd780b0d5465b1336b6f9ed28c2106d50a400
David Žižlavský <zizlavsky_david@seznam.cz>
2026-06-20 21:58:15 +0200

    GLFW and Vulkan test code

--- ExampleApp/src/main.cpp
+++ ExampleApp/src/main.cpp
@@ -1,9 +1,39 @@
 #include <iostream>
 #include "dummy.h"
+#include <GLFW/glfw3.h>
 
 int main()
 {
-    std::cout << "Example app started" << std::endl;
+    if (!glfwInit())
+    {
+        std::cout << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "RenderX Vulkan Window", nullptr, nullptr);
+
+    if (!window)
+    {
+        std::cout << "Failed to create GLFW window\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    std::cout << "GLFW window created\n";
+
     RenderX::init();
+
+    // Main loop
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
     return 0;
 }
--- RenderX/src/dummy.cpp
+++ RenderX/src/dummy.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
+#include <vulkan/vulkan.h>
 
 namespace RenderX
 {
     void init()
     {
-        std::cout << "RenderX initialized" << std::endl;
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "RenderX Test";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "RenderX";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_3;
+
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        VkInstance instance;
+
+        VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+
+        if (result != VK_SUCCESS)
+        {
+            std::cout << "Failed to create Vulkan instance!\n";
+            return;
+        }
+
+        std::cout << "Vulkan instance created successfully!\n";
+
+        vkDestroyInstance(instance, nullptr);
+
+        std::cout << "Vulkan instance destroyed successfully!\n";
     }
 }
